b853a2e85213dc6e17acfae36a74a6093865edf3
Alexander Ukwueze <71698748+lexizuchenna@users.noreply.github.com>
2023-06-09 00:28:26 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+ANDROID_HOME=C:\Users\Dr. Shedrack\Downloads\cmdline-tools (recommended setting)
+ANDROID_SDK_ROOT=C:\Users\Dr. Shedrack\Downloads\cmdline-tools (DEPRECATED)
+Using Android SDK: C:\Users\Dr. Shedrack\Downloads\cmdline-tools
+Could not find an installed version of Gradle either in Android Studio,
+or on your system to install the gradle wrapper. Please include gradle
+in your path, or install Android Studio
